87c4acee291eb356daa3af325ad2ba4397b2e659
ziyif <ziyif@andrew.cmu.edu>
2016-11-28 00:29:42 -0500

    9th commit
    small fixes compared to 8

--- squash/app/forms.py
+++ squash/app/forms.py
@@ -114,6 +114,6 @@ class UserForm(forms.ModelForm):
     
     class Meta:
         model=User
-        fields=['username','email']
+        fields=['username']
 
 
--- squash/app/views.py
+++ squash/app/views.py
@@ -270,7 +270,7 @@ class UserFormView(View):
 
             if user is not None:
                 if user.is_active:
-                    login(request,user)
+                    auth_login(request,user)
                     return redirect('signup')
 
         return render(request,self.template_name,{'form':form})
